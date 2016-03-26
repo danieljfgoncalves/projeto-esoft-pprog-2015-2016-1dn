423c43f4e37122adfadbda6d89ccf0420fe87b0d
Renato <1140822@isep.ipp.pt>
2016-03-26 21:30:37 +0000

    Resto do UC6 + CODIGO

---
+++ CÓDIGO_UC6/RegistarPerfilController.java
@@ -0,0 +1,23 @@
+public class RegistarPerfilController {
+
+	public void novoUtilizador() {
+		// TODO - implement RegistarPerfilController.novoUtilizador
+		throw new UnsupportedOperationException();
+	}
+
+	public void setDados() {
+		// TODO - implement RegistarPerfilController.setDados
+		throw new UnsupportedOperationException();
+	}
+
+	public void registaUtilizador() {
+		// TODO - implement RegistarPerfilController.registaUtilizador
+		throw new UnsupportedOperationException();
+	}
+
+	public void validaUser() {
+		// TODO - implement RegistarPerfilController.validaUser
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ CÓDIGO_UC6/Utilizador.java
@@ -0,0 +1,33 @@
+public class Utilizador {
+
+	private int nome;
+	private int email;
+	private int user;
+	private int password;
+
+	public void setNome() {
+		// TODO - implement Utilizador.setNome
+		throw new UnsupportedOperationException();
+	}
+
+	public void setEmail() {
+		// TODO - implement Utilizador.setEmail
+		throw new UnsupportedOperationException();
+	}
+
+	public void setUser() {
+		// TODO - implement Utilizador.setUser
+		throw new UnsupportedOperationException();
+	}
+
+	public void setPassword() {
+		// TODO - implement Utilizador.setPassword
+		throw new UnsupportedOperationException();
+	}
+
+	public void validaDados() {
+		// TODO - implement Utilizador.validaDados
+		throw new UnsupportedOperationException();
+	}
+
+}
