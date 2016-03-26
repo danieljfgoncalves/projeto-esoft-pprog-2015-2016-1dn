52362633caca3c899c433cb44c026c8a288ef75d
Renato <1140822@isep.ipp.pt>
2016-03-26 21:46:55 +0000

    CODIGO E DIAGRAMA CLASSES UC7

---
+++ CÓDIGO_UC7/AutenticarPerfilUI.java
@@ -0,0 +1,26 @@
+public class AutenticarPerfilUI {
+
+	public List getPerfisPendentes() {
+		// TODO - implement AutenticarPerfilUI.getPerfisPendentes
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param SelectedItem
+	 */
+	public Perfil getPerfil(int SelectedItem) {
+		// TODO - implement AutenticarPerfilUI.getPerfil
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param decisao
+	 */
+	public void autenticarPerfil(boolean decisao) {
+		// TODO - implement AutenticarPerfilUI.autenticarPerfil
+		throw new UnsupportedOperationException();
+	}
+
+}
